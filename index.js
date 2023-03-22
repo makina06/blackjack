05d659a1e7bbc0f16200136bd808a24249cc9e6f
Max <119096559+makina06@users.noreply.github.com>
2023-03-22 21:13:11 +1300

    completed the function startGame for the button START GAME

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("helloworld");
+let firstCard = 11;
+let secondCard = 2;
+let sum = firstCard + secondCard;
+let cardEl = document.getElementById("card-el");
+let sumEl = document.getElementById("sum-el");
+let playEl = document.getElementById("play-el");
+let message = "";
+
+function startGame() {
+  cardEl.textContent += " " + firstCard + " " + secondCard;
+  sumEl.textContent += " " + sum;
+  if (sum < 21) {
+    message = " You can pick another card";
+  } else if (sum === 21) {
+    message = " Blackjack ! Whoo-hoo!";
+  } else message = "You  are out of the game";
+
+  playEl.textContent = message;
+}
